4dd39ce8f0223baf1b580c13c9e6f2281c55fe37
jelimarl <jelimarlm@gmail.com>
2022-12-01 11:47:23 -0500

    Botón para agregar productos al pedido

--- burger-queen/src/components/specificProductCard.jsx
+++ burger-queen/src/components/specificProductCard.jsx
@@ -1,17 +1,23 @@
 import React from "react"
 import { Card, ListGroup } from "react-bootstrap"
 import '../styles/specificProductCard.css'
+import Button from "react-bootstrap/Button"
 
 function SpecificProductCard(props) {
 
+  function handleClick() {
+    console.log('Producto seleccionado', props.product.id)
+  }
+
   return (
-    <Card style={{ width: '20rem' }}>
+    <Card style={{ width: '15rem' }}>
       <Card.Img variant='top' src={props.product.image} className='cardImg' />
       <Card.Body>
         <Card.Title>{props.product.name}</Card.Title>
         <ListGroup variant='flush'>
           <ListGroup.Item>${props.product.price}</ListGroup.Item>
         </ListGroup>
+        <Button onClick={handleClick}>Agregar</Button>
       </Card.Body>
     </Card>
   )
