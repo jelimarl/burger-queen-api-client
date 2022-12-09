63c404aa06ff1769b33ea9a15ba5cdc862910a98
jelimarl <jelimarlm@gmail.com>
2022-12-08 20:24:54 -0500

    Reorganizando Vistas del Mesero

--- burger-queen/src/views/waiterView.jsx
+++ burger-queen/src/views/waiterView.jsx
@@ -1,16 +1,199 @@
-import React from "react";
-import { HeaderWaiter } from "../components/headerWaiter"
-import { Order } from "../components/order"
-
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import '../styles/waiterView.css'
+import { getProducts } from "../utils/petitions";
+import { SpecificProductCard } from "../components/specificProductCard";
+import { SelectedProductCard } from "../components/selectedProductCard";
+import Button from "react-bootstrap/Button";
+import Swal from 'sweetalert2';
+import { newOrder } from "../utils/petitions"
+import { HeaderWaiter } from "../components/headerWaiter";
 
 function WaiterView() {
+
+  const [completeList, setCompleteList] = useState([])
+
+  const [specificList, setSpecificList] = useState([])
+
+  const [selectedItem, setSelectedItem] = useState([])
+
+  const [customerName, setCustomerName] = useState('')
+
+  useEffect(() => {
+
+    getProducts()
+      .then((response) => {
+        // console.log(response.data)
+        setCompleteList(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  function handleChangeSelector(event) {
+
+    const result = completeList.filter((product) => {
+      if (event.target.value === product.type) {
+        return true
+      }
+      return false
+    })
+
+    setSpecificList(result)
+    // console.log(result)
+  }
+
+  console.log('Producto Seleccionado', selectedItem)
+
+  function purchaseTotal() {
+
+    if (selectedItem.length !== 0) {
+      const pricesArray = selectedItem.map((item) => item.product.price * item.qty)
+      const total = pricesArray.reduce(function (a, b) { return a + b; })
+      return `Total: $${total}`
+    }
+  }
+
+  function handleChange(event) {
+    // console.log(event.target.value)
+    setCustomerName(event.target.value)
+  }
+
+  function handleSubmit(event) {
+
+    event.preventDefault()
+    if (selectedItem.length !== 0) {
+      // console.log('Enviando Pedido')
+
+      newOrder(customerName, selectedItem)
+        .then((response) => {
+          console.log(response)
+
+          Swal.fire(
+            '¡Enviado!',
+            'El Pedido se ha enviado exitosamente.',
+            'success'
+          )
+          setSelectedItem([])
+          setCustomerName('')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '¡Agrega un producto al pedido!'
+      })
+    }
+  }
+
+  function handleClick() {
+
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás deshacer esta acción!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Sí, bórralo!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        setSelectedItem([])
+        setCustomerName('')
+
+        Swal.fire(
+          '¡Borrado!',
+          'El Pedido ha sido borrado exitosamente.',
+          'success'
+        )
+      }
+    })
+  }
+
   return (
+
     <div>
       <HeaderWaiter />
 
-      <div className="waiterView">
-        <Order />
-      </div>
+
+
+      <Form className="select-menu">
+        <Form.Select onChange={handleChangeSelector}>
+          <option>Escoge un Menú</option>
+          <option value='Desayuno'>Desayuno</option>
+          <option value='Almuerzo'>Almuerzo</option>
+        </Form.Select>
+      </Form>
+
+      <section className="orderSection">
+        <section className="products">
+          {
+            specificList.map((product, index) => {
+
+              return (
+                <div key={index}>
+                  <SpecificProductCard
+                    product={product}
+                    setSelectedItem={setSelectedItem}
+                    selectedItem={selectedItem}
+                  />
+                </div>
+              )
+            })
+          }
+        </section>
+
+        <section className="order">
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Control
+                className="nameInput"
+                type='text'
+                name='name'
+                placeholder="Nombre del Cliente"
+                value={customerName}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              {
+                selectedItem.map((item, index) => {
+
+                  return (
+                    <div key={index}>
+                      <SelectedProductCard
+                        item={item}
+                        index={index}
+                        setSelectedItem={setSelectedItem}
+                        selectedItem={selectedItem}
+                      />
+                    </div>
+                  )
+                })
+              }
+            </Form.Group>
+            <Form.Group className="total">
+              <p>{purchaseTotal()}</p>
+            </Form.Group>
+            <Button className="order-btn" type='submit'>
+              Enviar a Cocina
+            </Button>
+            <Button variant="outline-danger" onClick={handleClick}>
+              Borrar Pedido
+            </Button>
+          </Form>
+        </section>
+
+      </section>
     </div>
   )
 }
--- burger-queen/src/views/waiterViewStatusOrders.jsx
+++ burger-queen/src/views/waiterViewStatusOrders.jsx
@@ -1,16 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { getOrders } from "../utils/petitions";
+import { OrderCardWaiter } from "../components/orderCardWaiter";
+import { OrderDeliveredCard } from "../components/orderDeliveredCard";
 import { HeaderWaiter } from "../components/headerWaiter";
-import { OrderStatusWaiter } from "../components/orderStatusWaiter";
 
 function WaiterViewStatusOrders() {
-  return (
-    <div>
-      <HeaderWaiter />
+  const [orders, setOrders] = useState([])
+  const [updateOrders, setUpdateOrders] = useState(false)
+
+  useEffect(() => {
+
+    getOrders()
+      .then((response) => {
+        console.log(response.data)
+        setOrders(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [updateOrders])
+
+  const readyOrders = orders.filter((order) => {
+    if (order.status === "ready") {
+      return true
+    }
+    return false
+  })
+
+  const deliveredOrders = orders.filter((order) => {
+    if (order.status === "delivered") {
+      return true
+    }
+    return false
+  })
+
+  console.log('ready', readyOrders)
+  console.log('delivered', deliveredOrders)
+
+  if (readyOrders.length !== 0) {
+    return (
       <div>
-        <OrderStatusWaiter />
+        <HeaderWaiter />
+
+        <section className="pending-ready">
+          <section className="pending">
+            {
+              readyOrders.map((order, index) => {
+                return (
+                  <div key={index}>
+                    <OrderCardWaiter
+                      order={order}
+                      status="Listo"
+                      setUpdateOrders={setUpdateOrders}
+                      updateOrders={updateOrders}
+                    />
+                  </div>
+                )
+              })
+            }
+          </section>
+
+          <section className="ready">
+            {
+              deliveredOrders.map((order, index) => {
+                return (
+                  <div key={index}>
+                    <OrderDeliveredCard
+                      order={order}
+                      status="Entregado"
+                    />
+                  </div>
+                )
+              })
+            }
+          </section>
+        </section>
       </div>
-    </div>
-  )
+    )
+  }
+  else {
+    return (
+      <div>
+        <HeaderWaiter />
+
+        <section className="ready">
+          {
+            deliveredOrders.map((order, index) => {
+              return (
+                <div key={index}>
+                  <OrderDeliveredCard
+                    order={order}
+                    status="Entregado"
+                  />
+                </div>
+              )
+            })
+          }
+        </section>
+      </div>
+    )
+  }
 }
 
 export { WaiterViewStatusOrders }
